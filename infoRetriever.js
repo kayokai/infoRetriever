8e8d9fd1832f78210b41a7606ee0cbbc3fda5c87
ippen <flash_wave@icloud.com>
2023-11-23 23:49:46 +0900

    add 基本情報, printing without omitting

--- infoRetriever.js
+++ infoRetriever.js
@@ -3,6 +3,7 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+const util = require('util');
 
 const url = 'https://suumo.jp/chintai/jnc_000086387922/';
 
@@ -17,11 +18,12 @@ axios.get(url)
         const $ = cheerio.load(response.data);
 
         // 各要素からデータ取得
+        result["基本情報"] = propertyViewNoteToDic($)
         result["ランディングテーブル"] = headerAndDataToDict($, '.property_view_table  th')
         result["部屋の特長・設備"] = headerAndDataToDict($, '#bkdt-option  li')
         result["物件概要"] = headerAndDataToDict($, '.data_table  th')
 
-        console.log(result)
+        console.log(util.inspect(result, { showHidden: false, depth: null }))
     })
     .catch(error => {
         console.error('an error occured while getting the html with axios:', error);
@@ -33,14 +35,28 @@ function headerAndDataToDict($, selector) {
     $(selector).each(
         function (index, row) {
             // th elementを取得
-            let shaped_header = $(row).text()
+            let header = $(row).text()
             // row == th elementの次要素$(row).next()であるtd elementを取得
-            let shaped_data = $($(row).next()).text()
+            let data = $($(row).next()).text()
             // 整形
-            shaped_header = shaped_header.replace(/[\n\t]/g, '')
-            shaped_data = shaped_data.replace(/[\n\t]/g, '')
+            shaped_header = header.replace(/[\n\t]/g, '')
+            shaped_data = data.replace(/[\n\t]/g, '')
             content[shaped_header] = shaped_data
         }
     );
     return content
+}
+
+function propertyViewNoteToDic($) {
+    notes = []
+    $('.property_view_note-info span').each(
+        function (index, span) {
+            // th elementを取得
+            let note = $(span).text()
+            // 整形
+            // shaped_note = note.replace(/[\n\t]/g, '')
+            notes.push(note)
+        }
+    );
+    return notes
 }
